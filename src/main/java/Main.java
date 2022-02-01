4423c96f8440830164f4e9640072f04152b68e76
Andrii Yarych <flakerino@gmail.com>
2022-02-01 22:52:24 +0200

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("HELLO WORLD THIS IS ULKA");
+        System.out.println("HOW ARE YOU DOING TODAY!!!???");
     }
 }
